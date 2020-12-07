e8d3a3e6f757a9941e073acf013baaed226b664b
Sylvain Laporte <sylvain.laporte.2@umontreal.ca>
2020-12-06 21:43:41 -0500

    Ready for first run in Colab

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -3,6 +3,7 @@
 # date: 2020-12-08
 # object: Functions to preprocess data for DeepSDF.
 
+import json
 import trimesh
 import numpy as np
 import third_party.surface_point_cloud as spc
@@ -54,3 +55,65 @@ def get_mesh_point_cloud(mesh):
     """
     scan_count = 100
     return spc.get_surface_point_cloud(mesh, bounding_radius=1, scan_count=scan_count)
+
+def dump_samples(points, sdfs, output_file):
+    """Save samples to a JSON file.
+
+    Args:
+        points (np.ndarray): sample spatial points
+        sdfs (np.ndarray): respective SDF values
+        output_file (string): path to the output file
+    """
+    data = {
+	    "points": points.tolist(),
+	    "sdfs": sdfs.tolist()
+    }
+
+    with open(output_file, "w+") as f:
+	    json.dump(data, f)
+
+def main(input_file, output_file, nb_samples):
+    """Run data preprocessing from command line"""
+    import pyrender
+
+    # Load the mesh
+    mesh = trimesh.load(input_file)
+
+    # Normalize mesh to a unit sphere
+    mesh = normalize_to_unit_sphere(mesh)
+
+    # Sample spatial points
+    points, sdfs = sample_points(mesh, nb_samples=nb_samples)
+
+    print("size")
+
+    dump_samples(points, sdfs, output_file)
+
+    mesh_to_render = pyrender.Mesh.from_trimesh(mesh)
+    colors = np.zeros(points.shape)
+    colors[sdfs < 0, 2] = 1
+    colors[sdfs > 0, 0] = 1
+    point_cloud = pyrender.Mesh.from_points(points, colors=colors)
+    scene = pyrender.Scene()
+    scene.add(mesh_to_render)
+    scene.add(point_cloud)
+    pyrender.Viewer(scene, use_raymond_lighting=True, point_size=2)
+
+if __name__ == "__main__":
+    import argparse
+
+    # Setup command line arguments
+    parser = argparse.ArgumentParser(description="Sample spatial points from mesh")
+    parser.add_argument("--input", "-i", default="input-meshes/cube.obj",
+        help="path to input mesh")
+    parser.add_argument("--samples", "-s", default="500000",
+        help="path to input mesh")
+    args = parser.parse_args()
+
+    input_file = args.input
+    input_file_name = input_file.split(".")[0].split("/")[-1]
+    output_file = f"dataset/{input_file_name}-samples.json"
+    nb_samples = int(args.samples)
+
+    # Run sampling
+    main(input_file, output_file, nb_samples)
